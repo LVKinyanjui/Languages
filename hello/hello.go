70351548fea2ce135be8d16a17efac8f4bcffb16
LVKinyanjui <leslievictorius@gmail.com>
2024-08-23 19:08:34 +0300

    Learned how to use slices, arrays in go

--- hello/hello.go
+++ hello/hello.go
@@ -3,18 +3,19 @@ package main
 import (
 	"fmt"
 	"log"
+
 	"github.com/LVKinyanjui/Golang/greetings"
 )
 
 func main() {
-    // Set properties of the predefined Logger, including
-    // the log entry prefix and a flag to disable printing
-    // the time, source file, and line number.
-    log.SetPrefix("greetings: ")
-    log.SetFlags(0)
+	// Set properties of the predefined Logger, including
+	// the log entry prefix and a flag to disable printing
+	// the time, source file, and line number.
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
 
 	// Request a greeting message
-	message, err := greetings.Hello("")
+	message, err := greetings.Hello("Dinkleberg")
 
 	// If an error was returned, print to console and exit
 	if err != nil {
