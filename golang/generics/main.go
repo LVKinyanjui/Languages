8ee789100351290415b81b82a00533bc588f00ec
LVKinyanjui <leslievictorius@gmail.com>
2024-08-28 15:22:47 +0300

    Used interfaces to declare a reusable type constraint

--- golang/generics/main.go
+++ golang/generics/main.go
@@ -2,6 +2,11 @@ package main
 
 import "fmt"
 
+// Type Constraints as an interface
+type Number interface {
+	int64 | float64
+}
+
 func main() {
 	// Initialize a map for the integer values
 	ints := map[string]int64{
@@ -15,36 +20,14 @@ func main() {
 		"second": 26.99,
 	}
 
-	// fmt.Printf("Non-Generic Sums: %v and %v\n",
-	// 	SumInts(ints),
-	// 	SumFloats(floats))
-
 	fmt.Printf("Generic Sums: %v and %v\n",
-		SumIntsOrFloats[string, int64](ints),
-		SumIntsOrFloats[string, float64](floats))
-}
-
-// SumInts adds together the values of m.
-func SumInts(m map[string]int64) int64 {
-	var s int64
-	for _, v := range m {
-		s += v
-	}
-	return s
-}
-
-// SumFloats adds together the values of m.
-func SumFloats(m map[string]float64) float64 {
-	var s float64
-	for _, v := range m {
-		s += v
-	}
-	return s
+		SumIntsOrFloats(ints),
+		SumIntsOrFloats(floats))
 }
 
 // SumIntsOrFloats sums the values of map m. It supports both int64 and float64
 // as types for map values.
-func SumIntsOrFloats[K comparable, V int64 | float64](m map[K]V) V {
+func SumIntsOrFloats[K comparable, V Number](m map[K]V) V {
 	var s V
 	for _, v := range m {
 		s += v
