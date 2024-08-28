d13af6ce04382351e35154c9f9c276a41001084b
LVKinyanjui <leslievictorius@gmail.com>
2024-08-28 09:02:12 +0300

    Used __new__ to create the class instance

--- Python/MetaProgramming/frozen_json.py
+++ Python/MetaProgramming/frozen_json.py
@@ -1,12 +1,24 @@
 from collections import abc
+import keyword
 
 class FrozenJSON:
     """A read-only façade for navigating a JSON-like object
     using attribute notation
     """
+    def __new__(cls, arg):
+        if isinstance(arg, abc.Mapping):
+            return super().__new__(cls)
+        elif isinstance(arg, abc.MutableSequence):
+            return [cls(item) for item in arg]
+        else:
+            return arg        
 
     def __init__(self, mapping):
-        self.__data = dict(mapping)
+        self.__data = {}
+        for key, value in mapping.items():
+            if keyword.iskeyword(key):
+                key += '_'
+            self.__data[key] = value
 
     def __getattr__(self, name):
         try:
@@ -16,12 +28,3 @@ class FrozenJSON:
 
     def __dir__(self):
         return self.__data.keys()
-
-    @classmethod
-    def build(cls, obj):
-        if isinstance(obj, abc.Mapping):
-            return cls(obj)
-        elif isinstance(obj, abc.MutableSequence):
-            return [cls.build(item) for item in obj]
-        else:
-            return obj
