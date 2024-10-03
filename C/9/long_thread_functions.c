f1b64bb4049bae93e8fd7026182bf5d8f9f158f0
LVKinyanjui <leslievictorius@gmail.com>
2024-10-03 09:02:17 +0300

    Passed arguments to a thread function as a long

---
+++ C/9/long_thread_functions.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <pthread.h>
+
+void* do_stuff(void* param)
+{
+    long thread_no = (long)param;
+    printf("Thread number %ld\n", thread_no);
+    return (void*)(thread_no + 1);
+}
+
+int main()
+{
+    pthread_t threads[20];
+
+    long t;
+    for (t = 0; t < 3; t++) {
+        // Convert the long t value to a void pointer.
+        pthread_create(&threads[t], NULL, do_stuff, (void*)t);
+    }
+
+    void* result;
+    for (t = 0; t < 3; t++) {
+        pthread_join(threads[t], &result);
+        //Convert the return value to a long before using it.
+        printf("Thread %ld returned %ld\n", t, (long)result);
+    }
+    return 0;
+}
