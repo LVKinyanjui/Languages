26e7efdb21d9cd9b9dfea3391de7908cf2100bda
LVKinyanjui <leslievictorius@gmail.com>
2024-10-23 08:42:26 +0300

    Created a doubly linked list; used auto

--- Cpp/3/linked_lists.cpp
+++ Cpp/3/linked_lists.cpp
@@ -2,11 +2,16 @@
 
 struct Element {
     Element* next{};
+    Element* previous{};
     void insert_after(Element* new_element) {
         // The this pointer is sometimes useful for clarity
         // though is not strictly necessary here
         new_element->next = this->next;
         this->next = new_element;
+    }
+    void insert_before(Element* new_element) {
+        new_element->previous = this->previous;
+        this->previous = new_element;
     }
     char prefix[2];
     short operating_number;
@@ -25,13 +30,21 @@ int main() {
     trooper3.prefix[0] = 'L';
     trooper3.prefix[1] = 'S';
     trooper3.operating_number = 005;
+    trooper3.insert_before(&trooper2);
+    trooper2.insert_before(&trooper1);
 
-    for (Element *cursor=&trooper1; /*Ensures not nullptr*/cursor; cursor = cursor->next) {
+    for (auto *cursor=&trooper1; /*Ensures not nullptr*/cursor; cursor = cursor->next) {
         printf("Stormtrooper %c%c-%d\n",
             cursor->prefix[0],
             cursor->prefix[1],
             cursor->operating_number);
     }
 
+    for (auto *cursor=&trooper3; cursor; cursor = cursor->previous) {
+        printf("Stormtrooper %c%c-%d\n",
+            cursor->prefix[0],
+            cursor->prefix[1],
+            cursor->operating_number);
+    }
     return 0;
 }
