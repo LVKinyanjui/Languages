ea9144c6d3abda4ffe3dc9a276ec86015dc37d87
LVKinyanjui <leslievictorius@gmail.com>
2024-11-15 10:50:43 +0300

    Implemented another interface for the bank database

--- Cpp/5/bank.cpp
+++ Cpp/5/bank.cpp
@@ -5,6 +5,17 @@ struct Logger {
     virtual void log_transfer(long from, long to, double amount) = 0;
 };
 
+struct AccountDatabase {
+    virtual ~AccountDatabase() = default;
+    virtual void set_get_amounts(long id) = 0;
+};
+
+struct InMemoryAccountDatabase: AccountDatabase {
+    void set_get_amounts(long id) {
+        printf("Your balance: ");
+    }
+};
+
 struct FileLogger: Logger {
     void log_transfer(long from, long to, double amount) override {
 
@@ -13,13 +24,16 @@ struct FileLogger: Logger {
 };
 
 struct ConsoleLogger: Logger {
+    ConsoleLogger(const char* input) {this->input = input;}
     void log_transfer(long from, long to, double amount) override {
-        printf("%ld -> %ld: %f\n", from, to, amount);
+        printf("%s \n%ld -> %ld: %f\n", input, from, to, amount);
     }
+    const char* input;
 };
 
 struct Bank {
-    Bank(Logger & logger) : logger{ logger } {}
+    Bank(Logger & logger, AccountDatabase& database)
+    : logger{ logger }, database{ database } {}
 
     void make_transfer(long from, long to, double amount) {
         logger.log_transfer(from, to, amount);
@@ -27,11 +41,13 @@ struct Bank {
 
     private:
         Logger& logger;
+        AccountDatabase& database;
 };
 
 int main() {
-    ConsoleLogger logger;
-    Bank bank{ logger }; // constructor injection. fixed at object construction
+    ConsoleLogger logger { "Console Logger" };
+    InMemoryAccountDatabase database;
+    Bank bank{ logger, database }; // constructor injection. fixed at object construction
     bank.make_transfer(1000, 2000, 49.95);
     return 0;
 }
